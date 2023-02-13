288ce9301a654a7f12fdfda112488e2647542174
jscheetz06 <56905935+jscheetz06@users.noreply.github.com>
2023-02-13 17:13:24 -0600

    Made changes

--- RPS/Form1.Designer.cs
+++ RPS/Form1.Designer.cs
@@ -29,11 +29,214 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.picCPU = new System.Windows.Forms.PictureBox();
+            this.picPlayer = new System.Windows.Forms.PictureBox();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtCountDown = new System.Windows.Forms.Label();
+            this.txtRounds = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.countDownTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picCPU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCPU
+            // 
+            this.picCPU.Image = global::RPS.Properties.Resources.q;
+            this.picCPU.Location = new System.Drawing.Point(750, 86);
+            this.picCPU.Name = "picCPU";
+            this.picCPU.Size = new System.Drawing.Size(270, 246);
+            this.picCPU.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCPU.TabIndex = 0;
+            this.picCPU.TabStop = false;
+            // 
+            // picPlayer
+            // 
+            this.picPlayer.Image = global::RPS.Properties.Resources.q;
+            this.picPlayer.Location = new System.Drawing.Point(230, 86);
+            this.picPlayer.Name = "picPlayer";
+            this.picPlayer.Size = new System.Drawing.Size(270, 246);
+            this.picPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPlayer.TabIndex = 0;
+            this.picPlayer.TabStop = false;
+            // 
+            // btnRock
+            // 
+            this.btnRock.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRock.Location = new System.Drawing.Point(89, 124);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(92, 42);
+            this.btnRock.TabIndex = 2;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPaper.Location = new System.Drawing.Point(89, 203);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(92, 42);
+            this.btnPaper.TabIndex = 2;
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScissors.Location = new System.Drawing.Point(89, 279);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(92, 42);
+            this.btnScissors.TabIndex = 2;
+            this.btnScissors.Text = "Scissors";
+            this.btnScissors.UseVisualStyleBackColor = true;
+            this.btnScissors.Click += new System.EventHandler(this.btnScissors_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(875, 473);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(105, 42);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(535, 101);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(188, 25);
+            this.txtScore.TabIndex = 3;
+            this.txtScore.Text = "Player: 0 - CPU: 0";
+            // 
+            // txtCountDown
+            // 
+            this.txtCountDown.AutoSize = true;
+            this.txtCountDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCountDown.Location = new System.Drawing.Point(591, 193);
+            this.txtCountDown.Name = "txtCountDown";
+            this.txtCountDown.Size = new System.Drawing.Size(24, 25);
+            this.txtCountDown.TabIndex = 4;
+            this.txtCountDown.Text = "5";
+            // 
+            // txtRounds
+            // 
+            this.txtRounds.AutoSize = true;
+            this.txtRounds.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRounds.Location = new System.Drawing.Point(568, 304);
+            this.txtRounds.Name = "txtRounds";
+            this.txtRounds.Size = new System.Drawing.Size(99, 25);
+            this.txtRounds.TabIndex = 4;
+            this.txtRounds.Text = "Round: 3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-157, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Rock";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(326, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 17);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(-162, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Rock";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(872, 48);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 17);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "CPU";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(321, 48);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 17);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Player";
+            // 
+            // countDownTimer
+            // 
+            this.countDownTimer.Interval = 1000;
+            this.countDownTimer.Tick += new System.EventHandler(this.countDownTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1085, 564);
+            this.Controls.Add(this.txtRounds);
+            this.Controls.Add(this.txtCountDown);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRock);
+            this.Controls.Add(this.picPlayer);
+            this.Controls.Add(this.picCPU);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picCPU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picCPU;
+        private System.Windows.Forms.PictureBox picPlayer;
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissors;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtCountDown;
+        private System.Windows.Forms.Label txtRounds;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer countDownTimer;
     }
 }
 
--- RPS/Form1.cs
+++ RPS/Form1.cs
@@ -12,9 +12,225 @@ namespace RPS
 {
     public partial class Form1 : Form
     {
+
+        int rounds = 3;
+        int timerPerRound = 6;
+        bool gameOver = false;
+
+        string[] CPUchoiceList = { "rock", "paper", "scissors", "rock", "paper", "scissors" };
+
+        int randomNumber = 0;
+
+        Random rnd = new Random();
+
+        string CPUChoice;
+
+        string playerChoice;
+
+        int playerScore;
+        int CPUScore;
+
+
+
         public Form1()
         {
             InitializeComponent();
+
+            countDownTimer.Enabled = true;
+
+            playerChoice = "none";
+
+            txtCountDown.Text = "5";
+
         }
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.rock;
+            playerChoice = "rock";
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.paper;
+            playerChoice = "paper";
+        }
+
+        private void btnScissors_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.scissors;
+            playerChoice = "scissors";
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            playerScore = 0;
+            CPUScore = 0;
+            rounds = 3;
+            txtScore.Text = "Player: " + playerScore + " - CPU: " + CPUScore;
+
+            playerChoice = "none";
+
+            countDownTimer.Enabled = true;
+
+            picPlayer.Image = Properties.Resources.q;
+            picCPU.Image = Properties.Resources.q;
+
+            gameOver = false;
+
+        }
+
+        private void countDownTimerEvent(object sender, EventArgs e)
+        {
+            timerPerRound -= 1;
+
+            txtCountDown.Text = timerPerRound.ToString();
+
+            txtRounds.Text = "Rounds: " + rounds;
+
+            if (timerPerRound < 1)
+            {
+                countDownTimer.Enabled = false;
+
+                timerPerRound = 6;
+
+                randomNumber = rnd.Next(0, CPUchoiceList.Length);
+
+                CPUChoice = CPUchoiceList[randomNumber];
+
+                switch (CPUChoice)
+                {
+                    case "rock":
+                        picCPU.Image = Properties.Resources.rock;
+                        break;
+
+                    case "paper":
+
+                        picCPU.Image = Properties.Resources.paper;
+                        break;
+
+                    case "scissors":
+
+                        picCPU.Image = Properties.Resources.scissors;
+                        break;
+
+                }
+
+                if (rounds > 0)
+                {
+                    checkGame();
+                }
+                else
+                {
+                    if (playerScore > CPUScore)
+                    {
+                        MessageBox.Show("Congrats Dude, ya won!!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("CPU won.. you are a loser!");
+                    }
+
+                    gameOver = true;
+                }
+
+            }
+
+
+
+        }
+
+        private void checkGame()
+        {
+            if (playerChoice == "rock" && CPUChoice == "paper")
+            {
+
+                CPUScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Oh Snap CPU just beat yo butt becuase paper covers rock, yo!!");
+            }
+            else if (playerChoice == "scissors" && CPUChoice == "rock")
+            {
+
+                CPUScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Oh Snap CPU just beat yo butt becuase that rock crushed your scissors, yo!!");
+            }
+
+            else if (playerChoice == "paper" && CPUChoice == "scissors")
+            {
+
+                CPUScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Oh Snap CPU just beat yo butt becuase your paper got sliced up by the scissors , yo!!");
+            }
+            else if (playerChoice == "rock" && CPUChoice == "scissors")
+            {
+
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Nice move my dude, you just crushed those scissors up dawg.");
+            }
+
+            else if (playerChoice == "scissors" && CPUChoice == "paper")
+            {
+
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Nice move my dude, you just torn that paper to shreds.");
+            }
+
+            else if (playerChoice == "paper" && CPUChoice == "rock")
+            {
+
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Nice move my dude, you just covered that rock like a lil punk.");
+            }
+            else if (playerChoice == "none")
+            {
+                MessageBox.Show("Make a choice buddy!");
+            }
+            else
+            {
+                MessageBox.Show("Draw!");
+
+            }
+
+            startNextRound();
+        }
+
+
+        private void startNextRound()
+        {
+            if (gameOver == true)
+            {
+                return;
+            }
+
+            txtScore.Text = "Player: " + playerScore + " - " + "CPU: "  + CPUScore;
+
+            playerChoice = "none";
+
+            countDownTimer.Enabled = true;
+
+            picPlayer.Image = Properties.Resources.q;
+            picCPU.Image = Properties.Resources.q;
+
+        }
+
     }
 }
+
